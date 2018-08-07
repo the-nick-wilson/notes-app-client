9c671e4f3592ec41b14cfc5fcd4ddee023c2cb97
nwilson <nwilson@sumologic.com>
2018-08-07 10:43:06 -0600

    Adding settings page

--- src/Routes.js
+++ src/Routes.js
@@ -5,6 +5,7 @@ import Login from "./containers/Login";
 import Signup from "./containers/Signup";
 import NewNote from "./containers/NewNote";
 import Notes from "./containers/Notes";
+import Settings from "./containers/Settings";
 import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import NotFound from "./containers/NotFound";
@@ -15,8 +16,9 @@ export default ({ childProps }) =>
         <AppliedRoute path="/" exact component={Home} props={childProps} />
         <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
         <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
+        <AuthenticatedRoute path="/settings" exact component={Settings} props={childProps} />
         <AuthenticatedRoute path="/notes/new" exact component={NewNote} props={childProps} />
         <AuthenticatedRoute path="/notes/:id" exact component={Notes} props={childProps} />
         { /* Finally, catch all unmatched routes */ }
         <Route component={NotFound} />
-    </Switch>;
+    </Switch>
