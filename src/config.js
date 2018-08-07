13d5438e557e46415c3a67c07d9183e22c896390
nwilson <nwilson@sumologic.com>
2018-08-07 10:36:49 -0600

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-    MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
     s3: {
         REGION: "us-west-2",
-        BUCKET: "notes-app-uploads-nick2"
+        BUCKET: "notes-app-api-2-dev-attachmentsbucket-qwy7x2p2m96t"
     },
     apiGateway: {
         REGION: "us-west-2",
-        URL: "https://8liqw3hh6h.execute-api.us-west-2.amazonaws.com/prod"
+        URL: "https://oz7gph7fe7.execute-api.us-west-2.amazonaws.com/dev"
     },
     cognito: {
         REGION: "us-west-2",
-        USER_POOL_ID: "us-west-2_z9hWdxdD5",
-        APP_CLIENT_ID: "6m6i2l4h0dl3hm4piih1vukmrj",
-        IDENTITY_POOL_ID: "us-west-2:ca28cde1-ddf2-48a7-ab13-b2af31c6d356"
+        USER_POOL_ID: "us-west-2_A3JhX7Nok",
+        APP_CLIENT_ID: "6s64tc2v7u7levtlopfj05tkef",
+        IDENTITY_POOL_ID: "us-west-2:bb4adff8-c4d2-46e6-a096-a13133eaa508"
     }
+};
+
+const prod = {
+    s3: {
+        REGION: "us-west-2",
+        BUCKET: "notes-app-api-2-prod-attachmentsbucket-1u3d5objz11p3"
+    },
+    apiGateway: {
+        REGION: "us-west-2",
+        URL: "https://fqf9urci3a.execute-api.us-west-2.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-west-2",
+        USER_POOL_ID: "us-west-2_n7PT5ROtw",
+        APP_CLIENT_ID: "3fnj6feqktpuo9cn67pl3chg4b",
+        IDENTITY_POOL_ID: "us-west-2:ae271074-d3ae-430f-8b9b-b2f80b12b32b"
+    }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+    ? prod
+    : dev;
+
+export default {
+    // Add common config values here
+    MAX_ATTACHMENT_SIZE: 5000000,
+    ...config
 };
